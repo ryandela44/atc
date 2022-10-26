5a04e9c4de9a30c2024a781799d6767c71c31f90
MacBook Pro <macbookpro@Ryans-Macbook-Pro.local>
2022-10-25 21:20:17 -0400

    aircraf header setup

--- src/En-route/src/Aircraft.h
+++ src/En-route/src/Aircraft.h
@@ -1,1 +1,16 @@
 #pragma once
+
+class Airspace {
+public:
+private:
+    int x_coor;
+    int y_coor;
+    int z_coor;
+    void set_x_coor(int x_coor);
+    void set_y_coor(int y_coor);
+    void set_z_coor(int z_coor);
+public:
+    int get_x_coor();
+    int get_y_coor();
+    int get_z_coor();
+};
