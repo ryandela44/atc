01b5a942a32a5e50876ed7d4644af7af71562053
MacBook Pro <macbookpro@Ryans-Macbook-Pro.local>
2022-10-25 21:58:11 -0400

    basic aircraft and airspace implementation

--- src/En-route/src/Aircraft.cpp
+++ src/En-route/src/Aircraft.cpp
@@ -14,11 +14,25 @@ public:
     }
 
     void set_y_coor(int y_coor) {
+        this->y_coor = y_coor;
     }
 
-    void set_z_coor(int y_coor) {
-
+    void set_z_coor(int z_coor) {
+        this->z_coor = z_coor;
     }
+
+    int get_x_coor() {
+        return x_coor;
+    }
+
+    int get_y_coor() {
+        return y_coor;
+    }
+
+    int get_z_coor() {
+        return z_coor;
+    }
+
 private:
     int x_coor;
     int y_coor;
--- src/En-route/src/Airspace.cpp
+++ src/En-route/src/Airspace.cpp
@@ -5,6 +5,29 @@
  *      Author: coen320
  */
 
+class Airspace {
+private:
+    int x_space = 10000;
+    int y_space = 10000;
+    int z_space = 25000;
+    int sea_level = 15000;
+public:
+    Airspace() {
+
+    }
+
+    int get_x_space() {
+        return x_space;
+    }
+
+    int get_y_space() {
+        return y_space;
+    }
+
+    int get_z_space() {
+        return x_space + sea_level;
+    }
+};
 
 
 
