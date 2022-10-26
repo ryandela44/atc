83ea5d4dbe34849c02e65f06a0d1c0699b5f9cd1
MacBook Pro <macbookpro@Ryans-Macbook-Pro.local>
2022-10-26 19:27:45 -0400

    small modifications

--- src/Aircraft.cpp
+++ src/Aircraft.cpp
@@ -6,12 +6,14 @@
  */
 class Aircraft {
 public:
-    Aircraft(int id, int x_coor, int y_coor, int flight_level, int speed) {
+    Aircraft(int id, int x_coor, int y_coor, int z_coor, int x_speed,int y_speed,int z_speed) {
         this->id = id;
         this->x_coor = x_coor;
         this->y_coor = y_coor;
-        this->flight_level = flight_level;
-        this->speed = speed;
+        this->z_coor = z_coor;
+        this->x_speed = x_speed;
+        this->y_speed = y_speed;
+        this->z_speed = z_speed;
     }
 
     int get_id() {
@@ -26,18 +28,28 @@ public:
         return y_coor;
     }
 
-    int get_flight_level() {
-        return flight_level;
+    int get_z_coor() {
+        return z_coor;
     }
 
-    int get_speed() {
-        return speed;
+    int get_x_speed() {
+        return x_speed;
+    }
+
+    int get_x_speed() {
+        return y_speed;
+    }
+
+    int get_x_speed() {
+        return z_speed;
     }
 
 private:
     int id;
     int x_coor;
     int y_coor;
-    int flight_level;
-    int speed;
+    int z_coor;
+    int x_speed;
+    int y_speed;
+    int z_speed;
 };
